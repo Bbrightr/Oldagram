46320832ef32457365ddea4370d9d1e77f0431b7
Bright Obiri Gyan <74192474+Bbrightr@users.noreply.github.com>
2025-03-07 16:06:09 +0100

    Challenge Completed
    Instagram Clone

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ const posts = [
     }
 ]
 
-// const avatarEl = document.getElementById("avatar")
 const mainProfEl = document.getElementById("mainProf")
 let proSect = ""
 
@@ -60,41 +59,12 @@ for (let i = 0; i < posts.length; i++){
                     </div>
 
                     <div class="commenter">
-                        <p class="username" id="userN">${posts[i].username}</p>
-                        <p class="comment" id="userComm">${posts[i].comment}</p>
+                        <p id="userN"><span class="username">${posts[i].username}</span> ${posts[i].comment}</p>
                     </div>
                 </div>
                     
             </section>
         `
-    // const avaterEl = document.getElementById("avatar")
-    // document.getElementById("avatar").src = posts[i].avatar
-    
-
-    // const profNamEl = document.getElementById("profName")
-    // profNamEl.textContent = posts[i].name
-
-    // const profLocaEl = document.getElementById("profLoca")
-    // profLocaEl.textContent = posts[i].location
-
-    // const profPostEl = document.getElementById("profPost")
-    // profPostEl.textContent = posts[i].post
-
-    // const numberLikesEl = document.getElementById("numberLikes")
-    // numberLikesEl.textContent = posts[i].likes
-
-    // const userNEl = document.getElementById("userN")
-    // userNEl.textContent = posts[i].username
-
-    // const userCommEl = document.getElementById("userComm")
-    // userCommEl.textContent = posts[i].comment
-    // console.log(posts[i].avatar)
-    // console.log(posts[i].name)
-    // console.log(posts[i].location)
-    // console.log(posts[i].post)
-    // console.log(posts[i].likes)
-    // console.log(posts[i].username)
-    // console.log(posts[i].comment)
 
 
 }
